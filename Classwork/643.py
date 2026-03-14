4939e8c5cbbb8979c3c6bcdaa3a24b780b8eb177
Vitaly <vitalik65kirillov@yandex.ru>
2026-03-14 12:54:48 +1000

    Add twoSum solution

---
+++ Classwork/643.py
@@ -0,0 +1,10 @@
+def findMaxAverage(nums, k):
+    max_sum = sum(nums[:k])
+    current_sum = max_sum
+
+    for i in range(k, len(nums)):
+        current_sum += nums[i] - nums[i - k]
+        max_sum = max(max_sum, current_sum)
+
+    return max_sum / k
+print(findMaxAverage([1,12,-5,-6,50,3], 4))
